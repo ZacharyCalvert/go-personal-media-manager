36b9c66ab05fb09472f6136fe216f3f4a20e8ee9
Zach Calvert <ZachCalvert@gmail.com>
2020-01-19 21:07:11 -0600

    Evaluating github actions

--- old_database_test.go
+++ old_database_test.go
@@ -9,4 +9,8 @@ func TestReadOldDatabase(t *testing.T) {
 	if loaded["label1"].Sha256 != "label1" {
 		t.Errorf("Wanted label1, got %s", loaded["label1"].Sha256)
 	}
+	var label1 = loaded["label1"]
+	if len(label1.FileExtensions) != 2 {
+		t.Errorf("Missing extensions")
+	}
 }
